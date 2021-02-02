9fec74297af7cda60e7c287cdfd4ec4697c7d88d
dxv2k <9569076+dxv2k@users.noreply.github.com>
2021-02-02 17:18:09 +0700

    clone ref repo to this repo

---
+++ test_cv2.py
@@ -0,0 +1,13 @@
+import numpy as np 
+import cv2 
+import math 
+import os
+
+def main(): 
+    dir_name = os.getcwd()
+    model = cv2.imread(os.path.join(dir_name, 'reference/model.jpg'), 0)
+
+
+
+if __name__ == '__main__': 
+    main()
