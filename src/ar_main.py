9388d3aa8b88f05dc8303c09fafb310ddfeb2d0b
dxv2k <9569076+dxv2k@users.noreply.github.com>
2021-02-09 14:26:04 +0700

    fix indent, typo

--- src/ar_main.py
+++ src/ar_main.py
@@ -15,11 +15,17 @@ import math
 import os
 from objloader_simple import *
 
+import sys 
 import argparse
 from utils import hex_to_rgb, render, projection_matrix
 from custom_args import custom_args
 
 # DEFAULT_COLOR = (0, 0, 0)
+
+
+# # Generate Installer Guideline
+# nonbuffered_stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)
+# sys.stdout = nonbuffered_stdout
 
 # Gooey Decorator
 @Gooey(
@@ -80,7 +86,7 @@ def main():
     # Compute model keypoints and its descriptors
     kp_model, des_model = orb.detectAndCompute(model, None)
 
-   if args.object: 
+    if args.object: 
         obj = OBJ(args.object.name,swapyz=True)
     else: 
         obj = OBJ(os.path.join(dir_name, './models/fox.obj'), swapyz=True)  
@@ -162,67 +168,6 @@ def main():
     cv2.destroyAllWindows()
 
     return 0
-
-# def render(img, obj, projection, model, color=False):
-#     """
-#     Render a loaded obj model into the current video frame
-#     """
-#     vertices = obj.vertices
-#     scale_matrix = np.eye(3) * 3
-#     h, w = model.shape
-
-#     for face in obj.faces:
-#         face_vertices = face[0]
-#         points = np.array([vertices[vertex - 1] for vertex in face_vertices])
-#         points = np.dot(points, scale_matrix)
-#         # render model in the middle of the reference surface. To do so,
-#         # model points must be displaced
-#         points = np.array([[p[0] + w / 2, p[1] + h / 2, p[2]] for p in points])
-#         dst = cv2.perspectiveTransform(points.reshape(-1, 1, 3), projection)
-#         imgpts = np.int32(dst)
-#         if color is False:
-#             cv2.fillConvexPoly(img, imgpts, DEFAULT_COLOR)
-#         else:
-#             color = hex_to_rgb(face[-1])
-#             color = color[::-1]  # reverse
-#             cv2.fillConvexPoly(img, imgpts, color)
-
-#     return img
-
-# def projection_matrix(camera_parameters, homography):
-#     """
-#     From the camera calibration matrix and the estimated homography
-#     compute the 3D projection matrix
-#     """
-#     # Compute rotation along the x and y axis as well as the translation
-#     homography = homography * (-1)
-#     rot_and_transl = np.dot(np.linalg.inv(camera_parameters), homography)
-#     col_1 = rot_and_transl[:, 0]
-#     col_2 = rot_and_transl[:, 1]
-#     col_3 = rot_and_transl[:, 2]
-#     # normalise vectors
-#     l = math.sqrt(np.linalg.norm(col_1, 2) * np.linalg.norm(col_2, 2))
-#     rot_1 = col_1 / l
-#     rot_2 = col_2 / l
-#     translation = col_3 / l
-#     # compute the orthonormal basis
-#     c = rot_1 + rot_2
-#     p = np.cross(rot_1, rot_2)
-#     d = np.cross(c, p)
-#     rot_1 = np.dot(c / np.linalg.norm(c, 2) + d / np.linalg.norm(d, 2), 1 / math.sqrt(2))
-#     rot_2 = np.dot(c / np.linalg.norm(c, 2) - d / np.linalg.norm(d, 2), 1 / math.sqrt(2))
-#     rot_3 = np.cross(rot_1, rot_2)
-#     # finally, compute the 3D projection matrix from the model to the current frame
-#     projection = np.stack((rot_1, rot_2, rot_3, translation)).T
-#     return np.dot(camera_parameters, projection)
-
-# def hex_to_rgb(hex_color):
-#     """
-#     Helper function to convert hex strings to RGB
-#     """
-#     hex_color = hex_color.lstrip('#')
-#     h_len = len(hex_color)
-#     return tuple(int(hex_color[i:i + h_len // 3], 16) for i in range(0, h_len, h_len // 3))
 
 if __name__ == '__main__':
     main()
